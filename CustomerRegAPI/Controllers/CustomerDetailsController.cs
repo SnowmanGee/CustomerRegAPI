981ccb6237b27e88bd82b077d5742f3f7084d97d
SnowmanGee <andigallimore@gmail.com>
2022-01-23 11:56:55 +0000

    Original out of the box API model and controller removed.

---
+++ CustomerRegAPI/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerRegAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerDetailsController : ControllerBase { };
+}
