e61b90a4cc9379c04d75528b5081e3001049bb20
SnowmanGee <andigallimore@gmail.com>
2022-01-23 12:45:14 +0000

    BusinessLogic project with validation interface added.

---
+++ BusinessLogic/Interfaces/IValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessLogic.Interfaces
+{
+    public interface IValidation
+    {
+        bool ValidateString(string name, int minLength, int maxLength);
+
+        bool ValidatePolicyReference(string reference);
+
+        bool ValidateAgePlusEighteen(DateTime dob);
+
+        bool ValidateEMail(string address);
+    }
+}
