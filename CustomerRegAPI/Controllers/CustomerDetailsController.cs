271ef2d5b6751da51135acfbcfea6e7a6c66a3ba
SnowmanGee <andigallimore@gmail.com>
2022-01-23 12:38:46 +0000

    Summary comments added for all endpoints and helper.

--- CustomerRegAPI/Controllers/CustomerDetailsController.cs
+++ CustomerRegAPI/Controllers/CustomerDetailsController.cs
@@ -4,17 +4,29 @@ using CustomerRegAPI.Models;
 
 namespace CustomerRegAPI.Controllers
 {
+    /// <summary>
+    /// Customer API endpoints
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class CustomerDetailsController : ControllerBase
     {
         private readonly CustomerRegContext _context;
 
+        /// <summary>
+        /// Basic constructor injecting DbContext into controller
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="validation"></param>
         public CustomerDetailsController(CustomerRegContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Get all customer records currently held in db.
+        /// </summary>
+        /// <returns>The list of Customers</returns>
         // GET: api/CustomerDetails
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CustomerDetail>>> GetCustomerDetails()
@@ -22,6 +34,11 @@ namespace CustomerRegAPI.Controllers
             return await _context.CustomerDetails.ToListAsync();
         }
 
+        /// <summary>
+        /// Gets a singular customer detail from the id argument passed in
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Matching record or 404 if no match found</returns>
         // GET: api/CustomerDetails/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CustomerDetail>> GetCustomerDetail(int id)
@@ -36,6 +53,12 @@ namespace CustomerRegAPI.Controllers
             return customerDetail;
         }
 
+        /// <summary>
+        /// Updates a record currently held in the db for the id passed in
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="customerDetail"></param>
+        /// <returns>ActionResult with status code denoting logics outcome.</returns>
         // PUT: api/CustomerDetails/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
@@ -67,6 +90,11 @@ namespace CustomerRegAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Creates new customer record .
+        /// </summary>
+        /// <param name="customerDetail"></param>
+        /// <returns>StatusCode denoting the calls outcome.</returns>
         // POST: api/CustomerDetails
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
@@ -78,6 +106,11 @@ namespace CustomerRegAPI.Controllers
             return CreatedAtAction("GetCustomerDetail", new { id = customerDetail.CustomerID }, customerDetail);
         }
 
+        /// <summary>
+        /// Deletes customer record matching the id passed in
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>StatusCode denoting the outcome</returns>
         // DELETE: api/CustomerDetails/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCustomerDetail(int id)
@@ -95,6 +128,11 @@ namespace CustomerRegAPI.Controllers
         }
 
         #region Helpers
+        /// <summary>
+        /// Checks the CustomerDetail table to ensure a matching value is found.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Boolean True if matching record found. False if not.</returns>
         private bool CustomerDetailExists(int id)
         {
             return _context.CustomerDetails.Any(e => e.CustomerID == id);
