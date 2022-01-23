204fc7a8e092c2739d06b038ccf6352c963f3e22
SnowmanGee <andigallimore@gmail.com>
2022-01-23 12:30:37 +0000

    Standard scaffolding added to CustomerDetailsController.

--- CustomerRegAPI/Controllers/CustomerDetailsController.cs
+++ CustomerRegAPI/Controllers/CustomerDetailsController.cs
@@ -1,8 +1,104 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CustomerRegAPI.Models;
 
 namespace CustomerRegAPI.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
-    public class CustomerDetailsController : ControllerBase { };
+    public class CustomerDetailsController : ControllerBase
+    {
+        private readonly CustomerRegContext _context;
+
+        public CustomerDetailsController(CustomerRegContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CustomerDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerDetail>>> GetCustomerDetails()
+        {
+            return await _context.CustomerDetails.ToListAsync();
+        }
+
+        // GET: api/CustomerDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerDetail>> GetCustomerDetail(int id)
+        {
+            var customerDetail = await _context.CustomerDetails.FindAsync(id);
+
+            if (customerDetail == null)
+            {
+                return NotFound();
+            }
+
+            return customerDetail;
+        }
+
+        // PUT: api/CustomerDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomerDetail(int id, CustomerDetail customerDetail)
+        {
+            if (id != customerDetail.CustomerID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customerDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CustomerDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CustomerDetail>> PostCustomerDetail(CustomerDetail customerDetail)
+        {
+            _context.CustomerDetails.Add(customerDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomerDetail", new { id = customerDetail.CustomerID }, customerDetail);
+        }
+
+        // DELETE: api/CustomerDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomerDetail(int id)
+        {
+            var customerDetail = await _context.CustomerDetails.FindAsync(id);
+            if (customerDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.CustomerDetails.Remove(customerDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        #region Helpers
+        private bool CustomerDetailExists(int id)
+        {
+            return _context.CustomerDetails.Any(e => e.CustomerID == id);
+        }
+        #endregion
+    }
 }
